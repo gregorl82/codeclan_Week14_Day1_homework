181910fbd23a7dbb636ff3786c6e51dcec50d05d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-17 21:01:20 +0000

    Information for genre and release date added to ChartItem component

--- src/components/ChartItem.js
+++ src/components/ChartItem.js
@@ -9,7 +9,9 @@ function ChartItem(props) {
         <h3>{props.children['im:name'].label}</h3>
         <p>{props.children['im:artist'].label}</p>
       </div>
+      <p className="chart-item-genre">{props.children.category.attributes.term}</p>
       <p className="chart-item-price">{props.children['im:price'].label}</p>
+      <p className="chart-item-release-date">Release Date: {props.children['im:releaseDate'].attributes.label}</p>
       <audio className="chart-item-preview" controls src={props.children.link[1].attributes.href}></audio>
     </div>
   )
