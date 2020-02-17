a6bf0d1d164c262fc10e111e8fe7c266d36fd16f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-17 17:01:43 +0000

    Adds fetch method to ChartContainer and passes data to ChartList as prop

--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import ChartItem from './ChartItem';
 
-function ChartList(){
+function ChartList(props){
   return (
     <Fragment>
       <ChartItem />
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -3,11 +3,27 @@ import ChartList from '../components/ChartList'
 
 class ChartContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState( { data: data.feed.entry } ))
+    .catch(err => console.error)
+  }
+
   render() {
     return (
       <div className="chart-container">
-        <h1>UK Top 20</h1>
-        <ChartList />
+        <h1>UK Apple Music Top 20</h1>
+        <ChartList charts={this.state.data}/>
       </div>
     );
   }
