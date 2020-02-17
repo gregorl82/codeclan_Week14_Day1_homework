799d9796f24f8758e5f8bc2c51d38c1d20dd9aa2
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-17 21:14:41 +0000

    Addresses warnings by adding alt text to icon and removing Fragment import

--- src/components/ChartItem.js
+++ src/components/ChartItem.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 function ChartItem(props) {
   return (
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -27,7 +27,7 @@ class ChartContainer extends Component {
   render() {
     return (
       <div className="chart-container">
-        <h1><img id="apple-icon" src={this.state.icon}/>iTunes Store Top 20 Songs</h1>
+        <h1><img id="apple-icon" src={this.state.icon} alt=""/>iTunes Store Top 20 Songs</h1>
         <ChartList charts={this.state.data}/>
       </div>
     );
